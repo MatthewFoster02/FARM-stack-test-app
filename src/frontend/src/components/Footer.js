c59f2f09ab1f30748b351b782bca8f090209ae63
MatthewFoster02 <mattfoster02@outlook.com>
2023-02-09 21:23:16 +0100

    Updated Cars page

--- src/frontend/src/components/Footer.js
+++ src/frontend/src/components/Footer.js
@@ -1,7 +1,9 @@
 const Footer = () => {
     return (
-        <div>Footer</div>
-    );
-}
-
-export default Footer;
+      <div className="bg-black text-white flex justify-center items-center h-24">
+        <div>A FARM stack based application for cars</div>
+        </div>
+    )
+  }
+  export default Footer
+  
--- src/frontend/src/pages/Cars.js
+++ src/frontend/src/pages/Cars.js
@@ -1,10 +1,59 @@
 import Layout from "../components/Layout";
+import Card from "../components/Card";
+import { useState, useEffect } from "react";
 
-const Cars = () => {
+const Cars = () => 
+{
+    const [cars, setCars] = useState([]);
+    const [brand, setBrand] = useState('');
+    const [isPending, setIsPending] = useState(true);
+
+    useEffect(() =>
+    {
+        fetch(`http://localhost:8000/cars?brand={brand}`).then(res => res.json()).then(data =>
+        {
+            setCars(data);
+        });
+        setIsPending(false);
+    }, [brand]);
+
+    const onChangeBrand = (e) =>
+    {
+        setCars([]);
+        setBrand(e.target.value);
+        setIsPending(true);
+    }
+
     return (
         <Layout>
-            <div>
-                Cars
+            <h2 className="font-bold font-mono text-lg text-center my-4">Cars - {brand ? brand : 'All Brands'}</h2>
+            <div className="mx-8">
+                <label htmlFor="cars">Choose a brand: </label>
+                <select name="cars" id="cars" onChange={onChangeBrand}>
+                    <option value=''>All Brands</option>
+                    <option value='Fiat'>Fiat</option>
+                    <option value="Citroen">Citroen</option>
+                    <option value="Renault">Renault</option>                   
+                    <option value="Opel">Opel</option> 
+                </select>
+            </div>
+            <div className="mx-8">
+                {
+                    isPending && <div>
+                        <h2>Loading cars, brand: {brand}...</h2>
+                    </div>
+                }
+                <div className="grid grid-cols-2 gap-3 lg:grid-cols-4">
+                    {
+                        cars && cars.map((el) =>
+                        {
+                            return (
+                                <Card key={el._id} car = {el} />
+                            )
+                        }
+                        )
+                    }
+                </div>
             </div>
         </Layout>
     );
