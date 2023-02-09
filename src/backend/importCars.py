54357033c9bda16fad407e37cf756480bd948786
MatthewFoster02 <mattfoster02@outlook.com>
2023-02-09 15:58:10 +0100

    Populating mongoDB with sample data

--- src/backend/importCars.py
+++ src/backend/importCars.py
@@ -1,46 +1,43 @@
 import csv
 from fastapi.encoders import jsonable_encoder
 
-
-
 # dotenv environment variables
-from dotenv import dotenv_values
+from decouple import config
 
 from models import CarBase
 
-config = dotenv_values(".env")
-
-# 
+DB_URL = config('DB_URL', cast=str)
+DB_NAME = config('DB_NAME', cast=str)
+COLLECTION_NAME = config('COLLECTION_NAME', cast=str)
 
 # read csv
 with open("sample_data.csv",encoding='utf-8') as f:
     csv_reader = csv.DictReader(f)
     name_records = list(csv_reader)
 
-
-
 # Mongo db - we do not need Motor here
 from pymongo import MongoClient
 client = MongoClient()
 
+client = MongoClient(DB_URL)
+db = client[DB_NAME]
+cars = db[COLLECTION_NAME]
 
-
-client = MongoClient(config['DB_URL'])
-db = client[config['DB_NAME']]
-cars = db[config['COLLECTION_NAME']]
-
-
-for rec in name_records[51:250]:
-    
+i = 0
+for rec in name_records:
     try:
         rec['cm3'] = int(rec['cm3'])
         rec['price'] = int(rec['price'])
         rec['year'] = int(rec['year'])
 
-        if rec['price']>1000:
-            car = jsonable_encoder(CarBase(**rec))  
-            print("Inserting:",car)
-            cars.insert_one(car)
+        
+        car = jsonable_encoder(CarBase(**rec))  
+        cars.insert_one(car)
 
     except ValueError:
         pass
+    if i % 100 == 0:
+        print(f"{i} cars inserted!")
+    i += 1
+
+print("DONE!")
