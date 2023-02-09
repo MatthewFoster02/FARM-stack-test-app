5cd2d9afc24c93de7bb585e66dfc9c08e7fc593b
MatthewFoster02 <mattfoster02@outlook.com>
2023-02-09 13:43:44 +0100

    insert car route

--- src/backend/main.py
+++ src/backend/main.py
@@ -3,16 +3,20 @@ from decouple import config
 from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
 
+# Import all sub-routes and rename to specific route
+from routers.cars import router as cars_router
+
 DB_URL = config('DB_URL', cast=str)
 DB_NAME = config('DB_NAME', cast=str)
 COLLECTION_NAME = config('COLLECTION_NAME', cast=str)
 
 app = FastAPI()
+app.include_router(cars_router, prefix='/cars', tags=['cars'])
 
 @app.on_event('startup')
 async def startup_db_client():
     app.mongodb_client = AsyncIOMotorClient(DB_URL)
-    app.mongodb = app.mongodb_client(DB_NAME)
+    app.mongodb = app.mongodb_client[DB_NAME]
 
 @app.on_event('shutdown')
 async def shutdown_db_client():
--- src/backend/routers/cars.py
+++ src/backend/routers/cars.py
@@ -1,7 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Body, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from models import CarBase
 
 router = APIRouter()
 
 @router.get('/', response_description='List all cars')
 async def list_cars():
     return {'data': 'All cars will be here'}
+
+@router.post('/', response_description='Add new car')
+async def create_car(request: Request, car: CarBase = Body(...)):
+    car = jsonable_encoder(car)
+    new_car = await request.app.mongodb['cars2'].insert_one(car)
+    created_car = await request.app.mongodb['cars2'].find_one({'_id': new_car.inserted_id})
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_car)
