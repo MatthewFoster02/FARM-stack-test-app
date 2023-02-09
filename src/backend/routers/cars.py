363f678804dc62aec6d3df4adac8e28cd973a746
MatthewFoster02 <mattfoster02@outlook.com>
2023-02-09 15:05:24 +0100

    cars endpoint
    5 separate endpoints:
    
    Get all the cars matching the query
    
    Get an individual car by ID
    
    Post new car to the database
    
    Patch Update fields of an existing car
    
    Delete a car with given ID

--- src/backend/routers/cars.py
+++ src/backend/routers/cars.py
@@ -1,17 +1,57 @@
-from fastapi import APIRouter, Request, Body, status
+from fastapi import APIRouter, Request, Body, status, HTTPException
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-from models import CarBase
+from typing import Optional, List
+from models import CarBase, CarDB, CarUpdate
 
 router = APIRouter()
 
 @router.get('/', response_description='List all cars')
-async def list_cars():
-    return {'data': 'All cars will be here'}
+async def list_all_cars(
+    request:Request,
+    min_price:int=0,
+    max_price:int=100000,
+    brand:Optional[str]=None,
+    page:int=1,
+    results_per_page:int=25
+    ) -> List[CarDB]:
+    skip = (page - 1) * results_per_page
+    query = {
+        'price': {'$lt': max_price, '$gt': min_price}
+    }
+    if brand:
+        query['brand'] = brand
+    full_query = request.app.mongodb['cars1'].find(query).sort('_id', 1).skip(skip).limit(results_per_page)
+    results = [CarDB(**raw_car) async for raw_car in full_query]
+    return results
 
 @router.post('/', response_description='Add new car')
-async def create_car(request: Request, car: CarBase = Body(...)):
+async def create_car(request:Request, car:CarBase=Body(...)):
     car = jsonable_encoder(car)
     new_car = await request.app.mongodb['cars2'].insert_one(car)
     created_car = await request.app.mongodb['cars2'].find_one({'_id': new_car.inserted_id})
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_car)
+
+@router.get('/{id}', response_description='Get single car by ID')
+async def get_car_by_ID(id:str, request:Request):
+    car = await request.app.mongodb['cars2'].find_one({'_id': id})
+    if car is not None:
+        return CarDB(**car)
+    raise HTTPException(status_code=404, detail=f"Car with id: {id} not found")
+
+@router.patch('/{id}', response_description='Update car')
+async def update_car(id:str, request:Request, car:CarUpdate=Body(...)):
+    await request.app.mongodb['cars2'].update_one(
+        {'_id': id}, {'$set': car.dict(exclude_unset=True)}
+    )
+    car = await request.app.mongodb['cars2'].find_one({'_id': id})
+    if car is not None:
+        return CarDB(**car)
+    raise HTTPException(status_code=404, detail=f"Car with id: {id} not found")
+
+@router.delete('/{id}', response_description='Delete car')
+async def delete_car(id:str, request:Request):
+    delete_result = await request.app.mongodb['cars2'].delete_one({'_id': id})
+    if delete_result.deleted_count == 1:
+        return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
+    raise HTTPException(status_code=404, detail=f"Car with id: {id} not found")
