a231eb5edf607a12b6b08bc81da9501cf4b4cb6a
MatthewFoster02 <mattfoster02@outlook.com>
2023-02-11 13:07:33 +0100

    Authorization and protected routes

--- src/backend/authentication.py
+++ src/backend/authentication.py
@@ -24,8 +24,11 @@ class Authorization():
         return jwt.encode(payload, self.secret, algorithm='HS256')
     
     def decode_token(self, token):
+        #token = token[22:-1]
+        print(token)
+        #payload = jwt.decode(token, self.secret, algorithms=['HS256'])
         try:
-            payload = jwt.decode(token, self.secret, algorithm='HS256')
+            payload = jwt.decode(token, self.secret, algorithms=['HS256'])
             return payload['sub']
         except jwt.ExpiredSignatureError:
             raise HTTPException(status_code=401, detail='Signature has expired')
@@ -33,4 +36,5 @@ class Authorization():
             raise HTTPException(status_code=401, detail='Invalid token')
     
     def auth_wrapper(self, auth:HTTPAuthorizationCredentials=Security(security)):
+        print(auth.credentials)
         return self.decode_token(auth.credentials)
--- src/backend/routers/cars.py
+++ src/backend/routers/cars.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter, Request, Body, status, HTTPException
+from fastapi import APIRouter, Request, Body, status, HTTPException, Depends
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse, Response
 from typing import Optional, List
 
 from models.cars import CarBase, CarDB, CarUpdate
+from authentication import Authorization
 
 router = APIRouter()
+authorization = Authorization()
 
 @router.get('/', response_description='List all cars')
 async def list_all_cars(
@@ -24,20 +26,21 @@ async def list_all_cars(
     if brand:
         query['brand'] = brand
     
-    full_query = request.app.mongodb['cars2'].find(query).sort('_id', 1).skip(skip).limit(results_per_page)
+    full_query = request.app.mongodb['cars3'].find(query).sort('_id', 1).skip(skip).limit(results_per_page)
     results = [CarDB(**raw_car) async for raw_car in full_query]
     return results
 
 @router.post('/', response_description='Add new car')
-async def create_car(request:Request, car:CarBase=Body(...)):
+async def create_car(request:Request, car:CarBase=Body(...), userID=Depends(authorization.auth_wrapper)):
     car = jsonable_encoder(car)
-    new_car = await request.app.mongodb['cars2'].insert_one(car)
-    created_car = await request.app.mongodb['cars2'].find_one({'_id': new_car.inserted_id})
+    car['owner'] = userID
+    new_car = await request.app.mongodb['cars3'].insert_one(car)
+    created_car = await request.app.mongodb['cars3'].find_one({'_id': new_car.inserted_id})
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_car)
 
 @router.get('/{id}', response_description='Get single car by ID')
 async def get_car_by_ID(id:str, request:Request) -> CarDB:
-    car = await request.app.mongodb['cars2'].find_one({'_id': id})
+    car = await request.app.mongodb['cars3'].find_one({'_id': id})
 
     if car is not None:
         return CarDB(**car)
@@ -45,11 +48,15 @@ async def get_car_by_ID(id:str, request:Request) -> CarDB:
     raise HTTPException(status_code=404, detail=f"Car with id: {id} not found")
 
 @router.patch('/{id}', response_description='Update car')
-async def update_car(id:str, request:Request, car:CarUpdate=Body(...)):
-    await request.app.mongodb['cars2'].update_one(
+async def update_car(id:str, request:Request, car:CarUpdate=Body(...), userID=Depends(authorization.auth_wrapper)):
+    # Check user authorized to update car
+    await checkOwnerOrAdmin(id, request, userID)
+
+    await request.app.mongodb['cars3'].update_one(
         {'_id': id}, {'$set': car.dict(exclude_unset=True)}
     )
-    car = await request.app.mongodb['cars2'].find_one({'_id': id})
+
+    car = await request.app.mongodb['cars3'].find_one({'_id': id})
 
     if car is not None:
         return CarDB(**car)
@@ -57,10 +64,22 @@ async def update_car(id:str, request:Request, car:CarUpdate=Body(...)):
     raise HTTPException(status_code=404, detail=f"Car with id: {id} not found")
 
 @router.delete('/{id}', response_description='Delete car')
-async def delete_car(id:str, request:Request):
-    delete_result = await request.app.mongodb['cars2'].delete_one({'_id': id})
+async def delete_car(id:str, request:Request, userID=Depends(authorization.auth_wrapper)):
+    # Check user authorized to update car
+    await checkOwnerOrAdmin(id, request, userID)
+
+    delete_result = await request.app.mongodb['cars3'].delete_one({'_id': id})
 
     if delete_result.deleted_count == 1:
         return Response(status_code=status.HTTP_204_NO_CONTENT)
     
     raise HTTPException(status_code=404, detail=f"Car with id: {id} not found")
+
+async def checkOwnerOrAdmin(id:str, request:Request, userID:str):
+    # Get user using userID and car using id
+    user = await request.app.mongodb['users'].find_one({'_id': userID})
+    car_to_update = await request.app.mongodb['cars3'].find_one({'_id': id})
+
+    # Check that owner of car is user or user is admin
+    if(userID != car_to_update['owner'] and user['role'] != 'ADMIN'):
+        raise HTTPException(status_code=401, detail='Only owner of car or ADMIN can update an existing car')
