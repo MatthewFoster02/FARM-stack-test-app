b55df5d8914d18ec4cec3ae873dfe305552e99f5
MatthewFoster02 <mattfoster02@outlook.com>
2023-02-23 15:12:14 +0000

    Custom hook for authorization

--- src/frontend/src/pages/_app.js
+++ src/frontend/src/pages/_app.js
@@ -1,15 +1,18 @@
 import '@/styles/globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import { AuthProvider } from '@/context/AuthProvider';
 
 export default function App({ Component, pageProps }) {
     return (
-        <div className='flex flex-col justify-between items-stretch min-h-screen'>
-            <Header />
-            <div className='flex-1'>
-                <Component {...pageProps} />
+        <AuthProvider>
+            <div className='flex flex-col justify-between items-stretch min-h-screen'>
+                <Header />
+                <div className='flex-1'>
+                    <Component {...pageProps} />
+                </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
+        </AuthProvider>
     )
 }
