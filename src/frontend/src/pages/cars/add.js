f5d56dcd9919291e240e0063ed63880807f58ba4
MatthewFoster02 <mattfoster02@outlook.com>
2023-02-24 10:11:44 +0000

    basic add car route

--- src/frontend/src/pages/cars/add.js
+++ src/frontend/src/pages/cars/add.js
@@ -1,6 +1,10 @@
-const add = () => {
-    return (
-        <div>Add Car</div>
-    );
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { getCookie } from "cookies-next";
+
+export const getServerSideProps = ({req, res}) =>
+{
+    const jwt = getCookie('jwt', {req, res});
+    return {props: {jwt}};
 }
-export default add;
